073fecbb7df340fa4aaf350f9c5cd35d3209de34
Rachanacs <70090917+rachana-c-s@users.noreply.github.com>
2021-05-02 12:23:57 +0530

    inputoutput.updated

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+console.log(outputDiv)
+
 
 btnTranslate.addEventListener("click" ,function ClickEventHandler(){
-    console.log("clicked!")
-})
+    console.log("clicked!");
+    console.log("input",txtInput.value)
+    outputDiv.innerText = "ssssssss "  + txtInput.value;
+});
